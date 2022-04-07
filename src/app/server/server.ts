483f4df8a41c9e1211827c0a15de999f8aad23ea
Pedro Cruz <pedrovictor.lage@gmail.com>
2022-04-07 18:56:28 -0300

    fix port

--- src/app/server/server.ts
+++ src/app/server/server.ts
@@ -24,7 +24,7 @@ export const webServer = async (): Promise<Server> => {
   app.use(UnitRoutes)
   app.use(AssetRoutes)
 
-  const server = app.listen(process.env['APP_PORT'] || 3000, () => {
+  const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`WebServer running`)
   })
 
